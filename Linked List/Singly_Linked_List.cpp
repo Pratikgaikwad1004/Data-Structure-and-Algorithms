7edc346fc909d45c634499537270a20b88d5799f
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2022-08-29 17:04:45 +0530

    Insertion completed

--- Linked List/Singly_Linked_List.cpp
+++ Linked List/Singly_Linked_List.cpp
@@ -17,12 +17,56 @@ void create_linked_list(int data)
     head = newnode;
 }
 
+struct Node *insert_at_first(int data)
+{
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->data = data;
+    newnode->next = head;
+    head = newnode;
+    cout << "Element inserted." << endl;
+    return head;
+}
+
+struct Node *insert_at_last(int data)
+{
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->data = data;
+    struct Node *ptr = head;
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    ptr->next = newnode;
+    newnode->next = NULL;
+    return head;
+}
+
+struct Node *insert_at_index(int data, int index)
+{
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->data = data;
+    struct Node *p = head;
+    struct Node *q = head->next;
+    int i = 1;
+    while (i < index)
+    {
+        p = p->next;
+        q = q->next;
+        i++;
+    }
+    newnode->next = q;
+    p->next = newnode;
+    return head;
+}
+
 void display()
 {
-    while (head != NULL)
+    struct Node *ptr = head;
+    cout << "Elements - ";
+    while (ptr != NULL)
     {
-        cout << head->data << " ";
-        head = head->next;
+        cout << ptr->data << " ";
+        ptr = ptr->next;
     }
 }
 
@@ -35,6 +79,9 @@ int main()
         cout << "\n \n";
         cout << "1. Create linked list." << endl;
         cout << "2. Display linked list." << endl;
+        cout << "3. Insert at first." << endl;
+        cout << "4. Insert at last." << endl;
+        cout << "5. Insert at index." << endl;
         cout << "0. Exit." << endl;
         cin >> ch;
         switch (ch)
@@ -51,10 +98,27 @@ int main()
                 cin >> a;
                 create_linked_list(a);
             }
-
+            cout << "Linked list created." << endl;
             break;
         case 2:
             display();
+            break;
+        case 3:
+            cout << "Enter element - " << endl;
+            cin >> a;
+            insert_at_first(a);
+            break;
+        case 4:
+            cout << "Enter element - " << endl;
+            cin >> a;
+            insert_at_last(a);
+            break;
+        case 5:
+            cout << "Enter element - " << endl;
+            cin >> a;
+            cout << "Enter index - " << endl;
+            cin >> b;
+            insert_at_index(a, b);
             break;
         default:
             cout << "Invalid Input.";
